7070393e8ee5bed7c1290d3426e7a74ada7aaf15
Magna Marek <magna.marek@gmail.com>
2016-11-23 20:09:25 +0100

    mod: query execution via single methods

--- src/repository.ts
+++ src/repository.ts
@@ -3,14 +3,6 @@ import { Expansion } from "./expansion";
 import { EntityMetadata } from "./metadata";
 import { Query } from "./query";
 
-export interface IRepository<K, V> {
-    all(args: any): Promise<Map<K, V>>;
-    get(args: { key: K }): Promise<V>;
-    getMany(args: { keys: K[] }): Promise<Map<K, V>>;
-    save(entity: V): Promise<V>;
-    saveMany(entity: V[]): Promise<Map<K, V>>;
-}
-
 export module Repository {
     export interface IMapper<V, M> {
         /**
@@ -25,25 +17,23 @@ export module Repository {
 }
 
 /**
+ * A helper class for a common repository. Not very well documented (yet).
  * K = primitive type of id
  * V = actual type stored
  * M = type exposed to consumer
  */
-export class Repository<K, V, M> implements IRepository<K, M> {
+export class Repository<K, V, M> {
     protected _workspace: Workspace;
     protected _entityType: EntityMetadata;
     private _executedQueries = new Map<string, Query>();
     private _mapper: Repository.IMapper<V, M>;
-    private _queryExecuter: (q: Query) => Promise<any>;
 
     constructor(args: {
         entityType: EntityMetadata;
-        queryExecuter: (q: Query) => Promise<any>;
         workspace: Workspace;
         mapper?: Repository.IMapper<V, M>;
     }) {
         this._entityType = args.entityType;
-        this._queryExecuter = args.queryExecuter;
         this._workspace = args.workspace;
         this._mapper = args.mapper || {
             toInternal: (entity: M) => entity as any as V,
@@ -56,7 +46,7 @@ export class Repository<K, V, M> implements IRepository<K, M> {
     }): Promise<Map<K, M>> {
         args = args || {};
 
-        return this._execute(new Query.All({
+        return this.execute(new Query.All({
             entityType: this._entityType,
             expansions: args.expansion != null ? Expansion.parse(this._entityType, args.expansion) : []
         }));
@@ -66,7 +56,7 @@ export class Repository<K, V, M> implements IRepository<K, M> {
         key: K;
         expansion?: string;
     }): Promise<M> {
-        return this._execute(new Query.ByKey({
+        return this.execute(new Query.ByKey({
             entityType: this._entityType,
             expansions: args.expansion != null ? Expansion.parse(this._entityType, args.expansion) : [],
             key: args.key
@@ -77,46 +67,130 @@ export class Repository<K, V, M> implements IRepository<K, M> {
         keys: K[];
         expansion?: string;
     }): Promise<Map<K, M>> {
-        return this._execute(new Query.ByKeys({
+        return this.execute(new Query.ByKeys({
             entityType: this._entityType,
             expansions: args.expansion != null ? Expansion.parse(this._entityType, args.expansion) : [],
             keys: args.keys
         }));
     }
 
-    save(entity: M): Promise<M> {
-        throw "NotImplemented";
+    where(args: {
+        filters: { [property: string]: { toString(): string } };
+        expansion?: string;
+    }): Promise<Map<K, M>> {
+        let indexes = new Map<string, any>();
+
+        for (let property in args.filters) {
+            // todo: check if those indexes actually exist
+            indexes.set(property, args.filters[property]);
+        }
+
+        if (indexes.size == 0) {
+            return this.all({ expansion: args.expansion });
+        } else if (indexes.size == 1) {
+            return this.execute(new Query.ByIndex({
+                entityType: this._entityType,
+                expansions: args.expansion != null ? Expansion.parse(this._entityType, args.expansion) : [],
+                index: indexes.keys().next().value,
+                value: indexes.values().next().value
+            }));
+        } else {
+            return this.execute(new Query.ByIndexes({
+                entityType: this._entityType,
+                expansions: args.expansion != null ? Expansion.parse(this._entityType, args.expansion) : [],
+                indexes: indexes
+            }));
+        }
     }
 
-    saveMany(entity: M[]): Promise<Map<K, M>> {
-        throw "NotImplemented";
+    /**
+     * To be implemented by child class.
+     */
+    protected loadAll(q: Query.All): Promise<V[]> {
+        throw `loading all entities of ${this._entityType.name} is not supported`;
     }
 
-    protected _execute(query: Query): Promise<Map<K, M>> {
-        return new Promise<Map<any, any>>((resolve, reject) => {
+    /**
+     * To be implemented by child class.
+     */
+    protected loadOne(q: Query.ByKey): Promise<V> {
+        throw `loading one entity of ${this._entityType.name} by its primary key is not supported`;
+    }
+
+    /**
+     * To be implemented by child class.
+     */
+    protected loadMany(q: Query.ByKeys): Promise<V[]> {
+        throw `loading multiple entities of ${this._entityType.name} by their primary keys is not supported`;
+    }
+
+    /**
+     * To be implemented by child class.
+     */
+    protected loadByIndex(q: Query.ByIndex): Promise<V[]> {
+        throw `loading multiple entities of ${this._entityType.name} by their index '${q.index}' is not supported`;
+    }
+
+    /**
+     * To be implemented by child class.
+     */
+    protected loadByIndexes(q: Query.ByIndexes): Promise<V[]> {
+        throw `loading multiple entities of ${this._entityType.name} by multiple indexes is not supported`;
+    }
+
+    /**
+     * Execute a query which is first checked against cache availability.
+     * If its or a superset has been executed already, cached data is returned.
+     * Otherwise, data is loaded from the service (or whatever the inheritor implemented),
+     * which is then added and returned. 
+     */
+    protected execute(query: Query): Promise<Map<K, M>> {
+        return new Promise<Map<K, M>>((resolve, reject) => {
             if (this._executedQueries.has(query.toString()) || this._hasSupersetQueryOf(query)) {
                 this._workspace.execute(query).then(resolve, reject);
             } else {
-                this._queryExecuter(query).then(result => {
-                    try {
-                        this._executedQueries.set(query.toString(), query);
+                this._execute(query).then(result => {
+                    this._executedQueries.set(query.toString(), query);
+                    let map = new Map<K, M>();
+                    let keyName = this._entityType.primaryKey.name;
 
-                        if (result instanceof Map) {
-                            (result as Map<any, any>).forEach(v => this._workspace.add({ entity: v, type: query.entityType.name, expansion: query.expansions }));
-                            resolve(result);
-                        } else {
-                            this._workspace.add({ entity: result, type: query.entityType.name, expansion: query.expansions });
+                    result.forEach(entity => {
+                        let key = entity[keyName];
 
-                            let map = new Map<any, any>();
-                            map.set((query as Query.ByKey).key, result);
-                            resolve(map);
+                        try {
+                            let exposed = this._mapper.toExposed(entity);
+                            this._workspace.add({
+                                entity: entity,
+                                type: query.entityType.name,
+                                expansion: query.expansions
+                            });
+
+                            map.set(key, exposed);
+                        } catch (e) {
+                            console.warn(`failed adding part of payload for query ${query.toString()} @ entity #${key}: ${e}`);
                         }
-                    } catch (error) {
-                        reject(error);
-                    }
+                    });
+
+                    resolve(map);
                 }, reject);
             }
-        }).then(result => result._map(x => this._mapper.toExposed(x)));
+        });
+    }
+
+    private _execute(q: Query): Promise<V[]> {
+        if (q instanceof Query.All) {
+            return this.loadAll(q);
+        } else if (q instanceof Query.ByKey) {
+            return this.loadOne(q).then(entity => [entity]);
+        } else if (q instanceof Query.ByKeys) {
+            return this.loadMany(q);
+        } else if (q instanceof Query.ByIndex) {
+            return this.loadByIndex(q);
+        } else if (q instanceof Query.ByIndexes) {
+            return this.loadByIndexes(q);
+        } else {
+            return Promise.reject<any>(`unknown query type for query ${q.toString()}`);
+        }
     }
 
     private _hasSupersetQueryOf(query: Query): boolean {
